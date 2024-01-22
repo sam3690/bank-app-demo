fdc9170885c723c1f826b403d3be2065bb369f6f
sam3690 <usamabinayoub@gmail.com>
2024-01-22 11:45:16 +0500

    updated 22-01-24 11:45 am

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -19,7 +19,7 @@ const  [toggle, setToggle] = useState(false);
         </ul>
 
         <div className='sm:hidden flex flex-1 justify-center items-center'>
-          <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain" onClick={() => setToggle((prev) => !prev)} />
+          <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={() => setToggle((prev) => !prev)} />
 
           <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
               <ul className="list-none flex-col justify-end items-center flex-1">
